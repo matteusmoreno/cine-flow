d6a6a4b24f4ab5e82e2b953af4833f8b514aebae
Matteus Moreno <matteus_moreno@live.com>
2024-05-03 11:58:47 -0300

    início do projeto

---
+++ src/main/java/br/com/matteusmoreno/cineflow/CineFlowApplication.java
@@ -0,0 +1,13 @@
+package br.com.matteusmoreno.cineflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/matteusmoreno/cineflow/CineFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matteusmoreno.cineflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
